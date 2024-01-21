51753e1413a89d34c7fcf582ffb399949449f884
Raul Valls <raul.valls@aquilessolutions.com>
2024-01-21 10:39:48 +0100

    Add project files.

---
+++ backend/Controllers/ProductsController.cs
@@ -0,0 +1,92 @@
+﻿using backend.Context;
+using backend.Dtos;
+using backend.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // CRUD -> Create Read Update Delete
+
+        //Create
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] CreateUpdateProductDto dto)
+        {
+            var newProduct = new ProductEntity()
+            {
+                Brand = dto.Brand,
+                Title = dto.Title
+            };
+
+            await _context.Products.AddAsync(newProduct);
+            await _context.SaveChangesAsync();
+
+            return Ok("Product Saved successfully");
+        }
+
+        //Read
+        [HttpGet]
+        public async Task<ActionResult<List<ProductEntity>>> GetAllProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<ProductEntity>> GetProductByID([FromRoute] long id)
+        {
+            var product = await _context.Products.SingleOrDefaultAsync(x => x.Id == id);
+
+            if (product == null)
+                return NotFound("Product Not Found");
+            return Ok(product);
+        }
+
+        //Update
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> UpdateProduct([FromRoute] long id, [FromBody] CreateUpdateProductDto dto)
+        {
+            var product = await _context.Products.SingleOrDefaultAsync(x => x.Id == id);
+
+            if (product == null)
+                return NotFound("Product Not Found");
+
+            product.Title = dto.Title;
+            product.Brand = dto.Brand;
+
+            return Ok("Product Updated Successfully");
+        }
+
+        //Delete
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteProduct([FromRoute] long id)
+        {
+            var product = await _context.Products.SingleOrDefaultAsync(x => x.Id == id);
+
+            if (product == null)
+                return NotFound("Product Not Found");
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return Ok("Product Deleted Successfully");
+
+        }
+    }
+}
